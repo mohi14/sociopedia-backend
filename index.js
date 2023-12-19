8d0591228b7ec0070436dd9460309f3daa7fc645
mohi14 <mohiuddinkhan1409@gmail.com>
2023-12-19 22:24:40 +0600

    api created for the posts

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/users.js"
 import { register } from "./controllers/auth.js"
+import { createPost } from "./controllers/posts.js"
+import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -37,11 +39,12 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /*ROUTES WITH FILES */
-app.post("/auth/register", upload.single("picture"), register)
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/post", verifyToken, upload.single("picture"), createPost);
 
 /*ROUTES*/
 app.use("/api/auth", authRoutes)
-app.use("/users",userRoutes)
+app.use("/users", userRoutes)
 
 /*MONGOOSE SETUP */
 const PORT = process.env.PORT || 8000
